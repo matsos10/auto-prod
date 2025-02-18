4d4c339918fd7637a420f7e11ef556a70e37a232
matsos10 <124372592+matsos10@users.noreply.github.com>
2025-02-18 11:25:38 +0000

    Create import.js

---
+++ pages/import.js
@@ -0,0 +1,12 @@
+import CSVUploader from '../components/CSVUploader'
+
+const ImportPage = () => {
+  return (
+    <div>
+      <h2>Importação de Palavras-chave via CSV</h2>
+      <CSVUploader />
+    </div>
+  )
+}
+
+export default ImportPage
